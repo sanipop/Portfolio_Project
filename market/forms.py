e522f8b6a1dce0842bbb575d6499e572d215ef59
sanipop <67932273+sanipop@users.noreply.github.com>
2024-05-15 23:54:16 +0100

    Create 1 forms.py

---
+++ market/forms.py
@@ -0,0 +1,43 @@
+# Importing necessary modules for creating Flask forms.
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, HiddenField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+
+# Importing the User model from the market module.
+from market.models import User
+
+
+# Form class for user registration.
+class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists! Please try a different username')
+          
+    # Custom validation method to check if the email address already exists.
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email Address already exists! Please try a different email address')
+
+    #Form fields for username, email address, password, and password confirmation.
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Create Account')
+
+
+
+# Form class for user login.
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name:', validators=[DataRequired()])
+    password = PasswordField(label='Password:', validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
+
+# Form class for selling an item.
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase Item!')
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell Item!')
